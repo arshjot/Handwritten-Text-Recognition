0eb68778769f37aa2e9954c1d4b6c9dfdf4ffefc
arshjot <arshjot@live.com>
2019-01-13 22:01:42 +0800

    Add padding and prefetching

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -9,23 +9,49 @@ class DataGenerator:
         with open('../data/iam_h' + str(config.im_height) + '_w' + str(config.im_width) + '.pickle', 'rb') as f:
             data = pickle.load(f)
         train, val = data['train'], data['validation']
+        self.train_label, self.val_label = train['labels'], val['labels']
 
         self.train_dataset = tf.data.Dataset.from_tensor_slices(
             (train['images'], train['im_widths'], train['lab_lengths']))
-        train_label_dataset = tf.data.Dataset.from_sparse_tensor_slices(
-            tf.SparseTensor(train['labels'][0], train['labels'][1], train['labels'][2]))
+        train_label_dataset = tf.data.Dataset.from_generator(
+            self.train_generator, output_types=tf.int32, output_shapes=(tf.TensorShape([None])))
         self.train_dataset = tf.data.Dataset.zip((self.train_dataset, train_label_dataset))
         self.train_dataset = self.train_dataset.shuffle(buffer_size=500, seed=42)
 
         self.val_dataset = tf.data.Dataset.from_tensor_slices((val['images'], val['im_widths'], val['lab_lengths']))
-        val_label_dataset = tf.data.Dataset.from_sparse_tensor_slices(
-            tf.SparseTensor(val['labels'][0], val['labels'][1], val['labels'][2]))
+        val_label_dataset = tf.data.Dataset.from_generator(
+            self.val_generator, output_types=tf.int32, output_shapes=(tf.TensorShape([None])))
         self.val_dataset = tf.data.Dataset.zip((self.val_dataset, val_label_dataset))
 
-        self.iterator = tf.data.Iterator.from_structure(self.train_dataset.output_types,
-                                                        self.train_dataset.output_shapes)
+        self.train_dataset = self.train_dataset.padded_batch(
+            self.config.batch_size,
+            padded_shapes=((tf.TensorShape([self.config.im_width, self.config.im_height]),
+                            tf.TensorShape([]), tf.TensorShape([])), tf.TensorShape([None])),
+            padding_values=((tf.constant(0.0), tf.constant(0), tf.constant(0)), tf.constant(-1)))
+        self.train_dataset = self.train_dataset.prefetch(tf.contrib.data.AUTOTUNE)
+
+        self.val_dataset = self.val_dataset.padded_batch(
+            self.config.batch_size,
+            padded_shapes=((tf.TensorShape([self.config.im_width, self.config.im_height]), tf.TensorShape([]), tf.TensorShape([])), tf.TensorShape([None])),
+            padding_values=((tf.constant(0.0), tf.constant(0), tf.constant(0)), tf.constant(-1)))
+        self.val_dataset = self.val_dataset.prefetch(tf.contrib.data.AUTOTUNE)
+
+        self.iterator = tf.data.Iterator.from_structure(
+            self.train_dataset.output_types, self.train_dataset.output_shapes)
         self.training_init_op = self.iterator.make_initializer(self.train_dataset)
         self.validation_init_op = self.iterator.make_initializer(self.val_dataset)
+
+        self.num_classes = data['num_chars']
+        self.num_iterations_train = len(train['images']) // self.config.batch_size
+        self.num_iterations_val = len(val['images']) // self.config.batch_size
+
+    def train_generator(self):
+        for el in self.train_label:
+            yield el
+
+    def val_generator(self):
+        for el in self.val_label:
+            yield el
 
     def initialize(self, sess, is_train):
         if is_train:
@@ -39,8 +65,9 @@ class DataGenerator:
 
 def main():
     class Config:
-        im_height = 32
-        im_width = 320
+        im_height = 40
+        im_width = 800
+        batch_size = 2
 
     tf.reset_default_graph()
     sess = tf.Session()
@@ -55,7 +82,7 @@ def main():
     print(out_x_im.shape, out_x_im.dtype)
     print(out_x_w.shape, out_x_w.dtype)
     print(out_x_len.shape, out_x_len.dtype)
-    print(out_y[0].shape, out_y[0].dtype)
+    print(out_y.shape, out_y.dtype)
 
     data_loader.initialize(sess, is_train=False)
     out_x_im, out_x_w, out_x_len, out_y = sess.run([x_im, x_w, x_len, y])
@@ -63,7 +90,7 @@ def main():
     print(out_x_im.shape, out_x_im.dtype)
     print(out_x_w.shape, out_x_w.dtype)
     print(out_x_len.shape, out_x_len.dtype)
-    print(out_y[0].shape, out_y[0].dtype)
+    print(out_y.shape, out_y.dtype)
 
 
 if __name__ == '__main__':
